862bc66e7b7a3a3ed61f360cc47ff058f68fe9ab
thivco <thibault.vivier@epitech.eu>
2024-10-15 18:02:28 +0200

    Work on the navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -2,40 +2,43 @@
 export default {
   data() {
     return {
-    pages : {
-    home: { title: 'Home', path: '/home' },
-    projects: { title : "Projects", path: "/projects"},
-    about: { title: 'About', path: '/about' },
-    contact: { title: 'Contact', path: '/contact' },
-}
+      pages: {
+        home: { title: 'Home', anchor: '#home' },
+        projects: { title: "Projects", anchor: "#projects" },
+        about: { title: 'About', anchor: '#about' },
+        contact: { title: 'Contact', anchor: '#contact' },
+      }
     }
-    
   },
-
-    methods: {},
-    mounted() {
-    },
+  methods: {},
+  mounted() {
+  },
 }
 
 </script>
 
 <template>
   <p>Hello navbar</p>
-  <li v-for="page in pages">
-    <ul>{{page.title}}</ul>
-  </li>
+  <div id="navbar">
+    <div v-for="page in pages">
+      <a v-bind:href="page.anchor">{{ page.title }}</a>
+    </div>
+  </div>
 </template>
 
 <style>
-li {
+#navbar {
+  padding: 1rem;
   list-style: none;
   background: white;
   color: black;
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
 }
 
 ul {
- padding: 0;
- margin: 0; 
+  padding: 0;
+  margin: 0;
 }
-
 </style>
