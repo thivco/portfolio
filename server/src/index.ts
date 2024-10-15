32284f949726a81ccbb6583b98c98ea7b1f32f61
thivco <thibault.vivier@epitech.eu>
2024-10-15 15:20:54 +0200

    Added Dockerfile to server, edited clients, testing Hono !

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,16 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
+  console.log("Hello mdr")
   return c.text('Hello Ho yes!')
 })
+app.get('/toast', (c) => {
+  console.log("Toasted")
+  return c.text('hon!')
+})
 
-app.get('/couille', (c) => c.text('GET TA COUILLE MDRRR'))
+app.get('/api', (c) => {
+  return c.json({ message: 'Hello!' })
+})
 
 export default app
