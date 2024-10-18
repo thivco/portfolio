d58660e71465db88706e85f15701e71bfb41c5b8
thivco <thibault.vivier@epitech.eu>
2024-10-18 23:30:29 +0200

    Added CORS git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add . Form data is now passed to the backend, need to implement mongo

--- client/src/components/Contact.vue
+++ client/src/components/Contact.vue
@@ -3,9 +3,30 @@ export default {
   components: {},
   data() {
     return {
+      formData: {
+        email: "",
+        description: "",
+      }
     }
   },
   methods: {
+    async submitHandler() {
+      try {
+        const RESPONSE = await fetch("http://localhost:8585/api/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(this.formData)
+        })
+        const DATA = await RESPONSE.json()
+        console.log("Here is the data", DATA);
+        
+      } catch (error) {
+        console.error("Something wrong happened :", error);
+        
+      }
+    }
   },
   mounted() {
   },
@@ -17,16 +38,16 @@ export default {
   <div id="contact">
     <p>Here is the contact form, feel free to contact me to give me ideas !</p>
     <p>Please</p>
-    <form id="contactform" action="/" method="post">
-      <input type="email" name="mail" placeholder="Your mail adress" id="contactform__mail">
-      <textarea name="description" id="contactform__description">And here</textarea>
-      <input id="contactform__submit" type="submit" value="Send me a letter !">
+    <form @submit.prevent="submitHandler">
+      <!-- <form id="contactform" action="localhost:8585/api/post_comment" method="post"> -->
+      <input type="email" v-model="formData.email" placeholder="Your mail adress" id="contactform__mail">
+      <textarea v-model="formData.description" id="contactform__description">And here</textarea>
+      <button type="submit">Submit</button>
     </form>
   </div>
-  </template>
+</template>
 
 <style>
-
 #contactform__submit {
   width: 10rem;
   margin: 0 auto;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,54 @@
 import { Hono } from 'hono'
 import { MongoClient } from 'mongodb'
+import { cors } from "hono/cors"
 
-  
-  const client = new MongoClient('mongodb://'+ process.env.MONGO_INITDB_ROOT_USERNAME + ":" + process.env.MONGO_INITDB_ROOT_PASSWORD +'@localhost:27017');
-  // await client.connect();
-  const db = client.db('Testing');
-  const collection = db.collection('comments');
-  
-  collection.insertOne({ key: 'value' });
-  
-  const docs = collection.find().toArray();
-
-  console.log(docs);
-  
 
 const app = new Hono()
+
+
+const client = new MongoClient('mongodb://' + process.env.MONGO_INITDB_ROOT_USERNAME + ":" + process.env.MONGO_INITDB_ROOT_PASSWORD + '@localhost:27017');
+const db = client.db('Testing');
+const collection = db.collection('comments');
+
+
+app.use(
+  cors({
+    origin: 'http://localhost:9595'
+  })
+);
+
+client.connect()
+  .then(() => {
+    console.log("Connection succes yes ?");
+
+  })
+
+
+collection.insertOne({ key: 'value' });
+
+const docs = collection.find().toArray();
+
+console.log(docs);
+
+
+app.post("/api/submit", async (c) => {
+  console.log("CORS activated");
+  try {
+    const formData = await c.req.json();
+    const { email, description } = formData;
+    console.log('Received data:', formData);
+
+    return c.json({
+      message: 'Data received successfully',
+      data: { email, description }
+    }, 200);
+  } catch (error) {
+    console.error('HONO ! Error :', error);
+    return c.json({ error: 'Failed to process data' }, 500);
+  }
+ 
+}
+)
 
 app.get('/', (c) => c.text('Hello Bun!'))
 export default {
