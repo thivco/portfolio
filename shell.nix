43fe92c255cdde372a54079fed07acad8321a827
thivco <thibault.vivier@epitech.eu>
2024-10-18 14:37:30 +0200

    Updated nix-shell to only start docker daemon if not runnning

--- shell.nix
+++ shell.nix
@@ -2,7 +2,14 @@
 pkgs.mkShell {
   # nativeBuildInputs is usually what you want -- tools you need to run
   nativeBuildInputs = with pkgs.buildPackages; [ docker ];
+
   shellHook = ''
-  sudo dockerd > /dev/null 2>&1 &
+      if ! pgrep dockerd >/dev/null; then
+        echo "Starting Docker daemon."
+        sudo dockerd > /dev/null 2>&1 &
+        sleep 2
+      else
+        echo "Docker daemon already running."
+      fi
   '';
 }
