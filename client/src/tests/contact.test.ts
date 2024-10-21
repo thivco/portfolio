48df6508af2ed6af1c9c13925ece0e590f2ed5a0
thivco <thibault.vivier@epitech.eu>
2024-10-21 18:14:52 +0200

    Added mounted component to test, unit test enabled, will not work in CI unless context change

--- client/src/tests/contact.test.ts
+++ client/src/tests/contact.test.ts
@@ -1,12 +1,15 @@
 // contact.test.ts
 import Contact from '../components/Contact.vue'
 import {describe, it, expect, test} from 'vitest';
+import {mount} from "@vue/test-utils"
 
-// const wrapper = mount()
+const wrapper = mount(Contact, {
+  shallow:true
+})
 // Something about shallow mount here
 
 describe('Contact form', () => {
   test('Sends a request to the backend', () => {
-    expect(submitHandler()).not.toBe(1)
+    expect(wrapper.vm.submitHandler()).not.toBe(1)
   })
 })
