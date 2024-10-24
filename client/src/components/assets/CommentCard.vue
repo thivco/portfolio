2134c6746ac994573c0d3599cd9039cab3bd4a3a
thivco <thibault.vivier@epitech.eu>
2024-10-24 23:30:10 +0200

    Added comment card component

---
+++ client/src/components/assets/CommentCard.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+
+export default {
+  components: {},
+  data() {
+    return {
+    }
+  },
+  methods: {},
+  mounted() {
+  },
+}
+
+</script>
+
+<template>
+  <div id="contact">
+
+  </div>
+</template>
+
+<style>
+</style>
