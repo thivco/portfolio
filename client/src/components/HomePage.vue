6e3798401c9b520a27f76fc57cbba6c2df2dd3c7
thivco <thibault.vivier@epitech.eu>
2024-11-02 21:37:12 +0100

    Edited project variable and contact css

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -7,7 +7,7 @@ export default {
         projects: { title: "Projects", anchor: "#projects" },
         about: { title: 'About', anchor: '#about' },
         contact: { title: 'Contact', anchor: '#contact' },
-        github: {title: "Github", anchor:"https://github.com/thivco"},
+        github: { title: "Github", anchor: "https://github.com/thivco" },
         project1: { title: "Project1", anchor: "#project1" },
         project2: { title: 'Project2', anchor: '#project2' },
         project3: { title: 'Project3', anchor: '#project3' },
@@ -28,11 +28,11 @@ export default {
     <p id="homepage__subtitle">Fullstack developer, configuration</p>
     <div id="homepage">
       <!-- <div v-for="page in pages"> -->
-        <a class="homepage__link" :class="project.format" v-for="page in pages" v-bind:href="page.anchor" :id="'homepage__'+page.title.toLowerCase()"><span>{{ page.title }}</span></a>
+      <a class="homepage__link" :class="page.format" v-for="page in pages" v-bind:href="page.anchor"
+        :id="'homepage__' + page.title.toLowerCase()"><span>{{ page.title }}</span></a>
       <!-- </div> -->
     </div>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
